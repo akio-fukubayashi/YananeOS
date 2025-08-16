79b310aea562d514e42075f4db5ba590b8d99c00
Akio Fukubayashi <seshiria55@gmail.com>
2025-08-17 00:20:03 +0900

    Add common variable types

---
+++ src/common.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//type
+#define UINT8 unsigned char
+#define UINT16 unsigned short
+#define UINT32 unsigned int
+#define UINT64 unsigned long long
+#define INT8 signed char
+#define INT16 signed short
+#define INT32 signed int
+#define INT64 signed long long
+
+#define INT_PTR UINT32
+#define UINT_PTR UINT32
