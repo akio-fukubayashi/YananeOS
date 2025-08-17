a5767275e27a4381c0c576ec0eb163f2da6f4f2f
Akio Fukubayashi <seshiria55@gmail.com>
2025-08-18 00:06:41 +0900

    Add boot stage

--- boot/boot.asm
+++ boot/boot.asm
@@ -13,8 +13,8 @@ init:
     ; Init Stack
     mov sp, 0x7E00
 
-load_kernel:
-    ; CHS mode (Future versions will support LBA)
+load_second_stage:
+    ; CHS mode
     mov ah, 0x02            ; read sectors mode
     mov al, 1               ; Sector Count
 
@@ -23,12 +23,54 @@ load_kernel:
     mov cl, 2               ; Sector Number (Sectors from 2 onwards in the kernel)
     mov dh, 0               ; Head Number
     ;mov dl, [boot_drive]    ; Drive Number (Set by BIOS)
-    mov dl, 0x80
+    ;mov dl, 0x80
 
-    ; Load Memory ES:BX = 0x10000
-    mov bx, 0x1000
+    ; Load Memory ES:BX = 0x7E00
+    mov bx, 0x0
     mov es, bx
-    xor bx, bx
+    mov bx, 0x7E00
+    int 0x13
+
+    ; Read Failed (CF=0)
+    jc hang
+    jmp second_stage
+
+hang:
+    hlt
+    jmp hang
+
+; Fill 512 bytes
+times 510-($-$$) db 0
+
+; 2 bytes 0xAA55
+db 0x55
+db 0xAA
+
+; second staget (0x7E00)
+second_stage:
+
+enable_a20:
+    ; BIOS
+    mov ax, 0x2401
+    int 0x15
+    ; Failed
+    jc hang
+    ; Feature support 8042 keyboard controller
+
+load_kernel:
+    ; LBA mode
+    mov ah, 0x42            ; read hard disk
+   
+    ; Set Disk Address packet DS:SI = lba_disk_address_packet
+    mov si, 0x0000
+    mov ds, si
+    mov si, lba_disk_address_packet
+
+    ; Drive Settings(1 sector = 512 bytes)
+    ;mov dl, [boot_drive]    ; Drive Number (Set by BIOS)
+    ;mov dl, 0x80
+
+    ; Load Memory (0x100000)
     int 0x13
 
     ; Read Failed (CF=0)
@@ -59,11 +101,16 @@ protect_mode_entry:
     mov esp, 0x9FC00
 
     ; Call Kernel
-    jmp 0x10000
+    jmp 0x70000
 
-hang:
-    hlt
-    jmp hang
+; LBA
+lba_disk_address_packet:
+    db 0x10     ; packet Size
+    db 0        ; reserved
+    dw 1        ; number of sectors
+    dw 0x0000   ; buffer offset
+    dw 0x7000   ; buffer segment
+    dq 0xA      ; LBA sector number
 
 ; GDT (Flat Model)
 align 8
@@ -77,10 +124,6 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1   ; GDT Size -1
     dd gdt_start                 ; GDT Address
 
-; Fill 512 bytes
-times 510-($-$$) db 0
-
-; 2 bytes 0xAA55
-db 0x55
-db 0xAA
-    
+; Fill 5KB
+program_end:
+    times 5120-($-init) db 0
